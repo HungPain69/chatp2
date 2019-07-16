ba2a1f4785bb584f9416c1cf34febda6e6845e75
hungpain69 <hungpain69@gmail.com>
2019-07-16 11:03:08 +0700

    done activity chat

--- app/src/main/java/com/example/svmc_mp/ManHinhChat.java
+++ app/src/main/java/com/example/svmc_mp/ManHinhChat.java
@@ -1,5 +1,6 @@
 package com.example.svmc_mp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,16 +11,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.svmc_mp.DoiTuong.Users;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ManHinhChat extends AppCompatActivity {
 
     CircleImageView anh_avatar;
-    TextView ten_ban_be;
+    TextView ten_ban_chat;
 
 
     FirebaseUser firebaseUser;
@@ -48,6 +55,27 @@ public class ManHinhChat extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        ten_ban_chat = findViewById(R.id.ten_bb_toolbar_chat);
+
+        intent = getIntent();
+        String username = intent.getStringExtra("username");
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("users").child(username);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Users users = dataSnapshot.getValue(Users.class);
+                ten_ban_chat.setText(users.getHoTen());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
